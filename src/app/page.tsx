745c6d5a6fba37a961c03c38823437c045e28d56
Filipe Salles <Filipesallesdev@gmail.com>
2024-10-23 15:24:51 -0300

    fix: corrigindo LAG dos cards da Lading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,17 @@ import { featuresJobs } from '@/data/features';
 import FeaturesCard from '@/components/featuresCard/FeaturesCard';
 
 export default function Home() {
+
   return (
     <main>
       <header className={styles.header}>
         <Logo />
         <div className={styles.btnWrapper}>
-          <Navlink href='/newUser' size='medium' type='ghost' icon={<CiLogin />}>Entrar</Navlink>
+          <Navlink href='/signin' size='medium' type='ghost' icon={<CiLogin />}>Entrar</Navlink>
           <Navlink href='/newUser' size='medium' type='solid'>Cadastre-se</Navlink>
         </div>
       </header>
-      
+
       <section className={styles.hero}>
         <div className={styles.content}>
           <h1><span className={styles.wordShadow}>Organize</span> a internet, <span className={styles.wordShadow}>Encontre</span> o que importa</h1>
@@ -34,11 +35,11 @@ export default function Home() {
         <div className={styles.cardsWrapper}>
           {featuresJobs.map((feature, i) => (
             <FeaturesCard
-            key={i}
-            description={feature.description}
-            img={feature.img}
-            title={feature.title}
-            index={i}
+              key={i}
+              description={feature.description}
+              img={feature.img}
+              title={feature.title}
+              index={i}
             />
           ))}
         </div>
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.scss';
+
+function page() {
+    return (
+        <div>
+            Modal
+        </div>
+    )
+}
+
+export default page
