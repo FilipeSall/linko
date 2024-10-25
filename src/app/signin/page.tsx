520f83d8c3c93433640e575f824c56ecd012deb5
Filipe Salles <Filipesallesdev@gmail.com>
2024-10-25 15:54:05 -0300

    fix:svg background img works only in landingpage

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.scss';
+
+function page() {
+    return (
+        <main className={styles.container}>
+            /signin
+        </main>
+    )
+}
+
+export default page
