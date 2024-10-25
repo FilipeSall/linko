07b46eed9d724f4d157a336d902e9af5612c9e88
Filipe Salles <Filipesallesdev@gmail.com>
2024-10-25 16:00:17 -0300

    fix: animation lag

--- src/components/featuresCard/FeaturesCard.tsx
+++ src/components/featuresCard/FeaturesCard.tsx
@@ -12,7 +12,7 @@ function FeaturesCard({ description, img, title, index }: FeaturesCardsProps) {
         <motion.div
             initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6 }}
+            transition={{ duration: 0.3 }}
             whileHover={{ rotate: index % 2 === 0 ? 1 : -1, transition: { duration: 0.2 } }}
             viewport={{ once: true }}
             layoutId={`card-${index}`}
