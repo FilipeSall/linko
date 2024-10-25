a24685ac3f9490f9c52e7b54da87181108124282
Filipe Salles <Filipesallesdev@gmail.com>
2024-10-25 20:52:38 -0300

    fix: importando as fontes atravez do proprio next

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['firebasestorage.googleapis.com'],
     },
+    sassOptions: {
+        additionalData: `$var: red;`,
+    },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,23 @@
 import type { Metadata } from "next";
 import "../styles/globals.scss";
+import { Montserrat, Caveat, Poppins } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat'
+})
+
+const poppins = Poppins({
+  subsets:['latin'],
+  variable:'--font-poppins',
+  weight:['100', '200', '300', '400','500', '600', '700', '800', '900']
+  
+})
+
+const caveat = Caveat({
+  subsets:['latin'],
+  variable:'--font-caveat'
+})
 
 export const metadata: Metadata = {
   title: "Linko",
@@ -12,20 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="app-container">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="pt-br" className={`app-container ${montserrat.variable} ${caveat.variable} ${poppins.variable}`}>
       <body>
         {children}
       </body>
