88a77c56a72880730eb39cdc8f4b6eff267f60cc
Filipe Salles <Filipesallesdev@gmail.com>
2024-10-25 16:13:33 -0300

    change:images now are storaged in firebase store

--- src/data/features.tsx
+++ src/data/features.tsx
@@ -1,39 +1,32 @@
-import cookimg from '@/assets/features/cook.webp';
-import devimg from '@/assets/features/programmer.webp';
-import marketerimg from '@/assets/features/marketer.webp';
-import jornalistimg from '@/assets/features/jornalist.webp';
-import designerimg from '@/assets/features/designer.webp';
-import musicimg from '@/assets/features/music.webp';
-
 export const featuresJobs = [
     {
         title:'Designers',
-        img:designerimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/designer.webp?alt=media&token=a8cc3d47-9c42-4b9d-8137-d0a4ea9c046e',
         description:'Escolha de paletas de cores, criação de elementos de UI, tipografia e mais. Tudo organizado para facilitar seu processo criativo.'
     },
     {
         title:'Desenvolvedores',
-        img:devimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/programmer.webp?alt=media&token=adece1d4-0191-4a22-8262-76a450bf3691',
         description:'Frameworks, bibliotecas, documentação e ferramentas para otimizar seu código e acelerar o desenvolvimento.'
     },
     {
         title:'Influencers',
-        img:marketerimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/marketer.webp?alt=media&token=1e7e728c-c081-420e-980d-2737ccdd9cde',
         description:'Criação de conteúdo, gestão de redes sociais e análise de performance para expandir seu alcance e engajamento.'
     },
     {
         title:'Cozinheiros',
-        img:cookimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/cook.webp?alt=media&token=28e90806-2247-4c8b-9912-7f6ea91a0173',
         description:'Receitas, técnicas culinárias, planejamento de cardápios e dicas de organização para otimizar sua cozinha.'
     },
     {
         title:'Jornalistas',
-        img:jornalistimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/jornalist.webp?alt=media&token=5bf1ca91-1f71-4b1c-aab0-c7ab38923e59',
         description:'Fontes de pesquisa, verificação de fatos, criação de pautas e recursos para aprimorar suas investigações e reportagens.'
     },
     {
         title:'Músicos',
-        img:musicimg,
+        img:'https://firebasestorage.googleapis.com/v0/b/linko-52873.appspot.com/o/music.webp?alt=media&token=147c502b-7b6a-4211-9c43-68cc518031b6',
         description:'Explore IAs para composição musical e ferramentas para remixagem que impulsionam sua criatividade e aprimoram suas produções'
     }
 ]
